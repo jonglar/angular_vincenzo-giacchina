a5e4a5a9d16068db92af78c65b6fa9ee4591c82d
jonglar <romolo.porta3@gmail.com>
2021-03-01 08:31:36 +0100

    netflix-clone

---
+++ hello-world/deleteme.ts
@@ -0,0 +1,8 @@
+function diff(a:number):number {
+    return a-2;
+}
+
+let aux = 5;
+let b = diff(aux);
+
+console.log(4 / []);
---
+++ netflix-clone/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { SliderComponent } from './components/slider/slider.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SliderComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
