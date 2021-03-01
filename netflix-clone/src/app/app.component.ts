a408daed165692f0b8769a741e3d76f88d805c4b
jonglar <romolo.porta3@gmail.com>
2021-03-01 20:53:39 +0100

    .

--- netflix-clone/src/app/app.component.ts
+++ netflix-clone/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  videoTutorialUrl:string = 'https://www.youtube.com/watch?v=54km9J4Wh9s&t=66s&ab_channel=Programming%27sFun';
   title = 'netflix-clone';
   test:string = 'test1';
 }
